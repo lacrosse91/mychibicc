86b75cb0e217570275fc6714a82815acd248e7a9
Shoichiro Kawauchi <lacrosse91.ut@gmail.com>
2020-06-29 18:48:46 +0000

    add * / ()

--- main.c
+++ main.c
@@ -5,6 +5,9 @@
 #include<stdbool.h>
 #include<string.h>
 
+//
+// Tokenizer
+//
 typedef enum {
     TK_RESERVED, //記号
     TK_NUM, //int literal
@@ -98,9 +101,8 @@ Token *tokenize(void) {
             p++;
             continue;
         }
-
-        if (*p == '+' ||
-            *p == '-') {
+        // Punctuators
+        if (ispunct(*p)) {
                 cur = new_token(TK_RESERVED, cur, p++);
                 continue;
         }
@@ -118,30 +120,145 @@ Token *tokenize(void) {
     return head.next;
 }
 
+//
+// parser
+//
+
+typedef enum {
+    ND_ADD, // +
+    ND_SUB, // -
+    ND_MUL, // *
+    ND_DIV, // /
+    ND_NUM, // int
+} NodeKind;
+
+// ast node type
+typedef struct Node Node;
+struct Node {
+    NodeKind kind; // Node kind
+    Node *lhs; // left side
+    Node *rhs; // right side
+    long val; // if kind is ND_NUM, its value
+};
+
+static Node *new_node(NodeKind kind) {
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = kind;
+    return node;
+}
+
+static Node *new_binary(NodeKind kind, Node *lhs, Node *rhs) {
+    Node *node = new_node(kind);
+    node->lhs = lhs;
+    node->rhs = rhs;
+    return node;
+}
+
+static Node *new_num(int val) {
+    Node *node = new_node(ND_NUM);
+    node->val = val;
+    return node;
+}
+
+static Node *expr(void);
+static Node *mul(void);
+static Node *primary(void);
+
+// expr = mul ("+" mul | "-" mul)
+static Node *expr(void) {
+    Node *node = mul();
+
+    for(;;) {
+        if (consume('+')) {
+            node = new_binary(ND_ADD, node, mul());
+        } else if (consume('-')) {
+            node = new_binary(ND_SUB, node, mul());
+        } else {
+            return node;
+        }
+    }
+}
+
+// mul = primary ( "*" primary | "/" primary)*
+static Node *mul (void) {
+    Node *node = primary();
+    for(;;) {
+        if (consume('*')) {
+            node = new_binary(ND_MUL, node, primary());
+        } else if (consume('/')) {
+            node = new_binary(ND_DIV, node, primary());
+        } else {
+            return node;
+        }
+    }
+}
+
+// primary = "(" expr ")" | num
+static Node *primary(void) {
+    if (consume('(')) {
+        Node *node = expr();
+        expect(')');
+        return node;
+    }
+
+    return new_num(expect_number());
+}
+
+//
+// code generator
+//
+
+static void gen(Node *node) {
+    if (node->kind == ND_NUM) {
+        printf("  push %ld\n", node->val);
+        return;
+    }
+
+
+    gen(node->lhs);
+    gen(node->rhs);
+
+    printf("  pop rdi\n");
+    printf("  pop rax\n");
+
+    switch (node->kind) {
+    case ND_ADD:
+        printf("  add rax, rdi\n");
+        break;
+    case ND_SUB:
+        printf("  sub rax, rdi\n");
+        break;
+    case ND_MUL:
+        printf("  imul rax, rdi\n");
+        break;
+    case ND_DIV:
+        printf("  cqo\n");
+        printf("  idiv rdi\n");
+        break;
+    }
+
+    printf("  push rax\n");
+}
 int main(int argc, char **argv) {
   if (argc != 2)
     error("%s: invalid number of arguments", argv[0]);
 
+  // Tokennize and parse
   user_input = argv[1];
   token = tokenize();
+  Node *node = expr();
 
+
+  // print out the first half of assembly.
   printf(".intel_syntax noprefix\n");
   printf(".global main\n");
   printf("main:\n");
 
-  // The first token must be a number
-  printf("  mov rax, %ld\n", expect_number());
+  // tranverse AST to emit assembly
+  gen(node);
 
-  // ... followed by either `+ <number>` or `- <number>`.
-  while (!at_eof()) {
-    if (consume('+')) {
-      printf("  add rax, %ld\n", expect_number());
-      continue;
-    }
-    expect('-');
-    printf("  sub rax, %ld\n", expect_number());
-  }
-
+  // pop
+  printf("  pop rax\n");
   printf("  ret\n");
 
   return 0;
