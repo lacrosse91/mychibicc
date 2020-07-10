84eaeea64bac8c08722dc3a066e7b76f6059c38c
Shoichiro Kawauchi <lacrosse91.ut@gmail.com>
2020-07-10 00:37:50 +0000

    パーサーの導入

--- main.c
+++ main.c
@@ -5,6 +5,11 @@
 #include <stdlib.h>
 #include <string.h>
 
+
+//
+// Tokenizer
+//
+
 typedef enum {
   TK_RESERVED, // Keywords or punctuators
   TK_NUM,      // Integer literals
@@ -119,30 +124,116 @@ Token *tokenize(void) {
   return head.next;
 }
 
+//
+// parser
+//
+
+typedef enum {
+  ND_ADD, // +
+  ND_SUB, // -
+  ND_NUM, // Integer
+} NodeKind;
+
+// AST node type
+typedef struct Node Node;
+struct Node {
+  NodeKind kind; // Node kind
+  Node *lhs;     // Left-hand side
+  Node *rhs;     // Right-hand side
+  long val;      // Used if kind == ND_NUM
+};
+
+static Node *new_node(NodeKind kind) {
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = kind;
+    return node;
+}
+
+static Node *new_binary(NodeKind kind, Node *lhs, Node *rhs) {
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = kind;
+    node->lhs = lhs;
+    node->rhs = rhs;
+    return node;
+}
+
+static Node *new_num(int val) {
+  Node *node = new_node(ND_NUM);
+  node->val = val;
+  return node;
+}
+
+static Node *expr(void);
+static Node *mul(void);
+
+// expr = mul ("+" mul | "-" mul)
+static Node *expr(void) {
+    Node *node = mul();
+
+    for(;;) {
+        if (consume('+'))
+            node = new_binary(ND_ADD, node, mul());
+        else if (consume('-'))
+            node = new_binary(ND_SUB, node, mul());
+        else
+            return node;
+    }
+}
+
+// mul = num
+static Node *mul(void) {
+    return new_num(expect_number());
+}
+
+static void gen(Node *node) {
+    switch (node->kind) {
+    case ND_NUM:
+        printf("  push %ld\n", node->val);
+        return;
+    }
+
+    gen(node->lhs);
+    gen(node->rhs);
+
+    //スタックに積まれている値2つをポップ
+    printf("  pop rdi\n");
+    printf("  pop rax\n");
+
+
+    switch (node->kind) {
+    case ND_ADD:
+        printf("  add rax, rdi\n");
+        printf("  push rax\n");
+        return;
+    case ND_SUB:
+        printf("  sub rax, rdi\n");
+        printf("  push rax\n");
+        return;
+    }
+}
+
 int main(int argc, char **argv) {
   if (argc != 2)
     error("%s: invalid number of arguments", argv[0]);
 
+
+  // Tokenize and parse.
   user_input = argv[1];
   token = tokenize();
+  Node *node = expr();
 
+  // Print out the first half of assembly.
   printf(".intel_syntax noprefix\n");
   printf(".global main\n");
   printf("main:\n");
 
-  // The first token must be a number
-  printf("  mov rax, %ld\n", expect_number());
 
-  // ... followed by either `+ <number>` or `- <number>`.
-  while (!at_eof()) {
-    if (consume('+')) {
-      printf("  add rax, %ld\n", expect_number());
-      continue;
-    }
+  // Tranverse the AST to emit assembly.
+  gen(node);
 
-    expect('-');
-    printf("  sub rax, %ld\n", expect_number());
-  }
+  // A result must be at the top of the stack, so pop it
+  // to RAX to make it a program exit code.
+  printf("  pop rax\n");
 
   printf("  ret\n");
   return 0;
